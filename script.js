b33d1c0da959720fbd5b86ab98d76a86a2440d12
Priyadharshini-3 <110907661+Priyadharshini-3@users.noreply.github.com>
2022-10-22 19:27:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+// RESUME IN JSON FORMAT
+
+let myResume={
+"basics":
+{
+    "name":"PRIYADHARSHINI.R",
+    "e-mail":"dharshiniraj1811@gmail.com",
+    "phone number":8056833248,
+    "address":"No:2,Indira Street, 6th Cross, Sundar Nagar, KK Nagar",
+    "city":"Tiruchirappalli",
+    "pincode":"620021",
+    "state":"Tamilnadu",
+    "country":"India"
+},
+"education":
+    [
+{
+     "institution":"Jamal Mohamed College",
+    "department":"BSc-Mathematics",
+    "percentage":90
+}
+    ],
+"skills":[
+    {
+    "typewriting":"Both Junior and Senior Grade in English and Tamil",
+    "computer skills":"Computer Office Automation"
+}
+],
+"interests":[
+    {
+    "name":"drawing","cooking","songs","poetric"
+}
+],
+"languages":[
+    {
+        "language":"Tamil","English"
+    }
+]
+"statement":[
+    "I hereby declare that all the above furnished details are true to the best of my knowledge and belief" 
+]
+}
+console.log(myResume);
